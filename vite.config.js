6fc8947ea0e0338eb2fb75ea73fa24a4f9ac736d
baninations <baninations@gmail.com>
2024-02-06 01:16:11 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MB-Managment/",
+  base: "/mb-management/",
   plugins: [react()],
 });
